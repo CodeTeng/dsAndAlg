ac4d96e470c7f06aff08927270c3976b757df416
teng <teng2002823@outlook.com>
2022-09-03 23:57:38 +0800

    树节点

---
+++ src/main/java/com/lt/dsandalg/tree/traverse/TreeNode.java
@@ -0,0 +1,14 @@
+package com.lt.dsandalg.tree.traverse;
+
+/**
+ * @description: 树节点
+ * @author: ~Teng~
+ * @date: 2022/9/3 22:51
+ */
+public class TreeNode {
+    Object val;
+
+    TreeNode leftChild;
+
+    TreeNode rightChild;
+}
